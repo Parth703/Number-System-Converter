b566204a58eff4f8d08448ac121dfc77cd1d74bd
Parth703 <65958161+Parth703@users.noreply.github.com>
2020-08-02 08:43:41 +0530

    Update dceiamlotoatcl.py

--- dceiamlotoatcl.py
+++ dceiamlotoatcl.py
@@ -7,6 +7,7 @@ def decimal_to_octal(num):
         num = num//8
     remainder.reverse()
     return ''.join(map(str, remainder))
-num = int(input("Enter Decimal Value: "))
-ret = decimal_to_octal(num)
-print("Octal Value is: ", ret)
+def D_to_O():
+    num = int(input("Enter Decimal Value: "))
+    ret = decimal_to_octal(num)
+    print("Octal Value is: ", ret)
