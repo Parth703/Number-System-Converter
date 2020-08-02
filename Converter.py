807ce9696468be515b8df74b3ef0ead603f1dab9
Parth703 <65958161+Parth703@users.noreply.github.com>
2020-08-02 08:53:09 +0530

    Merger

---
+++ Converter.py
@@ -0,0 +1,8 @@
+from bnirayotdceiaml import B_to_D
+from oatclotdceiaml import O_to_D
+from hxeaotdceiaml import H_to_D
+from dceiamlotbniray import D_to_B
+from dceiamlotoatcl import D_to_O
+from dceiamlothxea import D_to_H
+
+while True:
